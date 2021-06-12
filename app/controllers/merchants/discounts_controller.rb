abc2cd9dd35e78e7de118323b02889afacdd6b8a
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-12 17:00:57 +0200

    Adds edit test and partial form with model

--- app/controllers/merchants/discounts_controller.rb
+++ app/controllers/merchants/discounts_controller.rb
@@ -9,4 +9,32 @@ class Merchants::DiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
   end
+
+  def edit
+    @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    discount.update(discount_params)
+    redirect_to "/merchants/#{@merchant.id}/discounts/#{discount.id}"
+    flash[:notice] = "Discount successfully updated!"
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.create!(discount_params)
+    redirect_to "/merchants/#{@merchant.id}/discounts"
+  end
+
+  private
+  def discount_params
+    params.require(:discount).permit(:percentage_discount, :quantity_threshold)
+  end
 end
